ffad2bff33be1aacad299f005f7a973f41edb0d9
Joel Ocay <jmocay72@gmail.com>
2020-05-19 20:23:44 -0400

    Binary search

---
+++ binary_search.py
@@ -0,0 +1,41 @@
+"""
+    Given a sorted array, find the index of a given value
+    in the array. Return -1 if the value is not in the array.
+"""
+def binary_search(arr, val):
+    lo = 0
+    hi = len(arr) - 1
+
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        print(lo, mid, hi)
+        if arr[mid] == val:
+            return mid
+        elif val < arr[mid]:
+            hi = mid - 1
+        else:
+            lo = mid + 1
+
+    return -1
+
+from datetime import datetime
+from random import seed, randint
+
+if __name__ == '__main__':
+
+    seed(datetime.now())
+
+    n = 100
+    arr = n * [0]
+    for i in range(n):
+        arr[i] = randint(1, 100)
+
+    arr.sort()
+
+    i = binary_search(arr, 23)
+    if i >= 0:
+        print(i, arr[i] == 23, arr[i])
+    else:
+        print('Not found.')
+
+    print('Not found: ', binary_search(arr, 150) == -1)
