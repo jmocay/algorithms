cc3dc4c2422722fa681efa5a47f9f51cd9096e3f
Joel Ocay <jmocay72@gmail.com>
2022-03-11 16:15:21 -0500

    added path

--- shortest_distance_using_bfs.py
+++ shortest_distance_using_bfs.py
@@ -2,20 +2,27 @@ import math
 
 def shortest_distance(graph, start, end):
     visited = {}
+    prev = {}
+
     q = [(start, 0)]
     visited.update({ start: None })
+    prev.update({ start: None })
+
     while len(q) > 0:
         current, dist = q.pop()
         if current == end:
-            return dist
+            return dist, build_path(prev, current)
         for neigh in graph[current]:
             if not neigh in visited:
                 q.insert(0, (neigh, dist+1))
                 visited.update({ neigh: None })
-    return math.inf
+                prev.update({ neigh: current })
+
+    return math.inf, []
 
 def build_graph(edges):
     graph = {}
+
     for edge in edges:
         a, b = edge
         if not a in graph:
@@ -24,7 +31,17 @@ def build_graph(edges):
             graph[b] = []
         graph[a].append(b)
         graph[b].append(a)
+
     return graph
+
+def build_path(prev, current):
+    path = [current]
+
+    while not prev[current] is None:
+        path.insert(0, prev[current])
+        current = prev[current]
+
+    return path
 
 if __name__ == '__main__':
     """
@@ -32,7 +49,7 @@ if __name__ == '__main__':
             a -- b -- e -- g
             |    |         |
             c -- d -- f -- +
-        Find the shortest distance from a to g 
+        Find the shortest distance from g to c 
     """
     edges = [
         ('a', 'b'),
@@ -45,7 +62,11 @@ if __name__ == '__main__':
         ('f', 'g'),
     ]
     graph = build_graph(edges)
+    begin, end = 'g', 'c'
+    dist, path = shortest_distance(graph, begin, end)
     print('Shortest distance between {} and {}: {}'.format(
-        'a', 'g',
-        shortest_distance(graph, 'a', 'g')
+        begin, end, dist
+    ))
+    print('Shortest path from {} to {}: {}'.format(
+        begin, end, path
     ))
